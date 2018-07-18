8d0613560837fe1314776bc515e27920334ac141
David Tai <david@verus.io>
2018-07-18 17:29:27 -0500

    login and signup pages

--- public/js/app.js
+++ public/js/app.js
@@ -28963,9 +28963,20 @@
     window.Shop = Shop$1;
   }
 
-  // src/js/shop.coffee
+  var Shop$2 = Shop$1;
 
-  // Shop.start settings
+  // src/js/shop.coffee
+  var settings$2;
+
+  settings$2 = {
+    // live key
+    // key: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJiaXQiOjQ1MDM2MTcwNzU2NzUxNzIsImp0aSI6Ii1YdHE0cHJVdDNrIiwic3ViIjoiTzVUQWtKcjBzZSJ9.4N1r2FxixxCh4DkGMGA_Iqg4VCNgFOK36Pd-3Zt1BXcqhbUFahU-CnfbeM7eIb01udcvGE2yPQRjAH2DjpRRcA'
+
+    // test key
+    key: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJiaXQiOjQ1MDM2MTcwNzU2NzUxNzYsImp0aSI6IkdxaUZ4dlFhZ1VJIiwic3ViIjoiTzVUQWtKcjBzZSJ9.E7Lc69XRmz-tq1U42aFqECLtcx_pv4w5NTGWeXqoLbM-o-uHcuM_2xS4_kJifA8LXIlLm2tx2M-CmJMdmeZuzA'
+  };
+
+  Shop$2.start(settings$2);
 
   // src/js/app.coffee
 
